be0edfaeea8f8e0cfca45167cff057b8413be3fa
Paul Beeler <pbeeler80@gmail.com>
2014-11-01 08:28:07 -0600

    Update to build date 11-01-14
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

---
+++ lib/gcc/arm-eabi/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = ".././../gcc/gcc-SaberMod/configure --prefix=/home/sabermod/tmp/arm-eabi-4.9 --target=arm-eabi --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/sabermod/sm-tc/build/temp-install --with-mpfr=/home/sabermod/sm-tc/build/temp-install --with-mpc=/home/sabermod/sm-tc/build/temp-install --with-cloog=/home/sabermod/sm-tc/build/temp-install --with-isl=/home/sabermod/sm-tc/build/temp-install --disable-ppl-version-check --disable-cloog-version-check --disable-isl-version-check --enable-cloog-backend=isl --enable-version-specific-runtime-libs --enable-threads --disable-shared --enable-target-optspace --disable-libssp --disable-libitm --with-abi=aapcs --with-float=soft --with-fpu=neon --disable-libstdc__-v3 --disable-libgomp --prefix=/home/sabermod/tmp/arm-eabi-4.9 --with-sysroot=/ --with-gcc-version=SaberMod --with-pkgversion=SaberMod-4.9.3_arm-graphite --with-binutils-version=android --with-gmp-version=gnu --with-mpfr-version=gnu --with-mpc-version=gnu --with-gdb-version=android --with-cloog-version=0.18.2 --with-isl-version=0.13 --enable-graphite=yes --enable-plugins --enable-threads --enable-ld=default --disable-option-checking --program-transform-name='s&^&arm-eabi-&'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "float", "soft" }, { "fpu", "neon" } };
